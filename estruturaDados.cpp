b70a26619c43eac2177ab171b3fdcc76ca675e07
Raul Pereira <92689246+rauldpereira@users.noreply.github.com>
2024-08-08 20:28:01 -0300

    mostrarTabela

--- estruturaDados.cpp
+++ estruturaDados.cpp
@@ -2,13 +2,74 @@
 
 using namespace std;
 
+void showTable()
+{
+    char letra[16] = "ABCDEFGHIJKLMNO";
+    cout << "  ";
+    for (int i = 0; i < 15; i++)
+    {
+        if (i < 9)
+            cout << "0"<< i + 1 << " ";
+        else
+            cout << i + 1 << ' ';
+
+    }
+    cout << endl;
+    for (int j = 0; j < 15; j++)
+    {
+        cout << letra[j] << ' ';
+        for(int i = 0; i < 15; i++){
+            cout << "[ ]";
+        }
+        cout << endl;
+    }
+}
+
+void matrizesVetores()
+{
+    cout << "Matrizes/Vetores\n";
+    int op = 0;
+
+    while (op != 4)
+    {
+        cout << "Qual modo deseja jogar? \n\
+1 - Player vs Player\n\
+2 - Player vs CPU \n\
+3 - CPU vs CPU\n\
+4 - Sair.\n";
+        cin >> op;
+
+    switch (op)
+    {
+        case 1:
+            cout << "Player vs Player\n";
+            showTable();
+            break;
+        case 2:
+            cout << "Player vs CPU\n";
+            showTable();
+            break;
+        case 3:
+            cout << "CPU vs CPU\n";
+            showTable();
+            break;
+        case 4:
+            cout << "Saindo...";
+            break;
+        default:
+            cout << "OpÃ§Ã£o Invalida.\n";
+            break;
+    }
+    }
+}
+
 int main(void)
 {
     int op = 0;
 
     while (op != 5)
     {
-        cout << "Escolha uma opção: \n\
+        cout << "Escolha uma opÃ§Ã£o: \n\
 1 - P1 - Matrizes/vetores\n\
 2 - P2 \n\
 3 - P3 \n\
@@ -20,7 +81,7 @@ int main(void)
         switch (op)
         {
             case 1:
-                cout << "Matrizes/Vetores\n";
+                matrizesVetores();
                 break;
             case 2:
                 cout << "oi\n";
@@ -35,7 +96,7 @@ int main(void)
                 cout << "Saindo...";
                 break;
             default:
-                cout << "Opção Invalida.";
+                cout << "OpÃ§Ã£o Invalida.\n";
                 break;
         }
     }
